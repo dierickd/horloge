9061ab1851626494461151ba66a34b0da3fd21a4
dewi <dierickd@outlook.fr>
2024-01-14 13:39:08 +0100

    wip

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// init variables
+typeHours = 12;
+
+
 // Initialisation après chargement du DOM
 document.addEventListener('DOMContentLoaded', function () {
     const oJauges = document.querySelectorAll('.progress-circle');
@@ -16,29 +20,37 @@ function createJauge(elem) {
         // création des éléments
         const oMask = document.createElement('div');
         const oBarre = document.createElement('div');
-        const oSup50 = document.createElement('div');
+        const oSup = document.createElement('div');
         const clock = document.createElement('div');
         // affectation des classes
         oMask.className = 'progress-masque';
         oBarre.className = 'progress-barre';
-        oSup50.className = 'progress-sup';
+        oSup.className = 'progress-sup';
         // construction de l'arbre
-        for (let i = 0; i < 12; i++) {
+        for (let i = 0; i < typeHours * 2; i++) {
             createHours(i, clock);
         }
 
         // test pie chart
-        const pie = document.createElement('div');
-        pie.classList.add("pieID");
-        pie.classList.add("pie");
+        // const pie = document.createElement('div');
+        // pie.classList.add("pieID");
+        // pie.classList.add("pie");
         // pie.setAttribute('data-value', '50');
         //
         oMask.appendChild(oBarre);
-        oMask.appendChild(oSup50);
+        oMask.appendChild(oSup);
         elem.appendChild(clock);
         elem.appendChild(oMask);
-        elem.appendChild(pie);
-        createPie([718, 531, 868, 344, 1145], ".pieID.pie");
+        // elem.appendChild(pie);
+        createPie(
+            [
+                ['22:00', '23:30'],
+                ['14:00', '17:00'],
+                ['16:00', '18:00'],
+                ['19:00', '23:00'],
+                ['12:00', '13:00'],
+            ],
+            ".pieID.pie");
     }
     return elem;
 }
@@ -48,37 +60,84 @@ function createHours(i, clock) {
     const timeNumber = document.createElement('div');
     clock.classList.add('clock');
     hour.classList.add('hour');
+    hour.classList.add('type-hour-' + typeHours);
     timeNumber.classList.add('time_number');
-    if (i === 11) {
-        timeNumber.innerText = (i + 1).toString() + 'h - 0h';
-    } else {
-        timeNumber.innerText = (i + 1).toString() + 'h';
+    if (i % 2 !== 0) {
+        timeNumber.innerText = Math.floor(i / 2 + 1).toString() + 'h';
     }
     hour.appendChild(timeNumber);
     clock.appendChild(hour);
 }
 
 function initJauge(elem) {
-    let oBarre;
-    let angle;
-    let valeur;
-    //
     createJauge(elem);
-    oBarre = elem.querySelector('.progress-barre');
-    valeur = elem.getAttribute('data-value');
-    valeur = valeur ? valeur * 1 : 0;
-    elem.setAttribute('data-value', valeur.toFixed(1));
-    angle = 360 * valeur / 100;
+    const hrDecimal = getHourDecimal();
+    const oBarre = elem.querySelector('.progress-barre');
 
-    if (oBarre) {
-        const nInterval = setInterval(() => {
-            valeur += 0.1;
-            console.log('loop');
-            if (valeur > 99.9) clearInterval(nInterval);
-            angle = 360 * valeur / 100;
-            elem.setAttribute('data-value', valeur.toFixed(1));
-
-            oBarre.style.transform = 'rotate(' + angle + 'deg)';
-        }, 864); // 60min => 3600000ms
-    }
+    refresh(elem, oBarre, hrDecimal);
+    const nInterval = setInterval(() => {
+        // if (hrDecimal > 99.9) clearInterval(nInterval);
+        refresh(elem, oBarre, hrDecimal);
+    }, 1000);
 }
+
+/**
+ *
+ * @param elem
+ * @param oBarre
+ */
+const refresh = (elem, oBarre) => {
+    const hrDecimal = getHourDecimal();
+    let angle = getAngle(hrDecimal);
+    oBarre = elem.querySelector('.progress-barre');
+
+    elem.setAttribute('data-value', hrDecimal.toFixed(1));
+    oBarre.style.transform = 'rotate(' + angle + 'deg)';
+};
+
+/**
+ *
+ * @returns {number}
+ */
+const getHourDecimal = () => {
+    const ratio = 100 / typeHours; //100% / 24 heures ou 12 heures
+    let hrs = new Date().getHours(); //récupére l'heure actuelle
+    const min = new Date().getMinutes(); //récupére les minutes actuelles
+
+    // si le typeHours est en 12h alors on converti l'heure ex: 14:32 => 2:32
+    if (typeHours === 12) {
+        hrs = formatHours12_24(hrs);
+    }
+    // converti les heures/minute en decimal => ex: 8:30 en 8.5
+    const hrDecimal = Number(hrs + '.' + Math.round(min / 60 * 100));
+    return hrDecimal * ratio;
+};
+
+function formatHours12_24(hrs) {
+    hrs = hrs % 12;
+    hrs = hrs ? hrs : 12; // the hour '0' should be '12'
+    return hrs;
+}
+
+/**
+ *
+ * @param hrDecimal
+ * @returns {number}
+ */
+const getAngle = (hrDecimal) => {
+    return hrDecimal * 360 / 100;
+};
+
+
+// arc.X (100): axe X => 100, correspond à la largeur de la forme
+// arc.Y (100): axe Y => 100, correspond à la hauteur de la forme
+// --------------------
+// lineTo.X (50): correspond à départ du trait axe x
+// lineTo.Y (50): correspond à départ du trait axe y
+// --------------------
+// radius correspont à l'angle radius de l'arc de cercle, 50 correspond à rayon du cercle 100 (arc.x et arc.y)
+
+// ctx.arc(100, 100, 50, degToRad(10), degToRad(20), false);
+// ctx.fillStyle = colors[1];
+// ctx.lineTo(100, 100);
+// ctx.fill();
--- pieChart.js
+++ pieChart.js
@@ -1,45 +1,44 @@
-const $ = jQuery;
+// const $ = jQuery;
 
-const sliceSize = (dataNum, dataTotal) => {
-    return (dataNum / dataTotal) * 360;
-}
-
-const addSlice = (sliceSize, pieElement, offset, sliceID, color) => {
-    $(pieElement).append("<div class='slice " + sliceID + "'><span></span></div>");
-    offset = offset - 1;
-    let sizeRotation = -179 + sliceSize;
-    $("." + sliceID).css({
-        "transform": "rotate(" + offset + "deg) translate3d(0,0,0)"
-    });
-    $("." + sliceID + " span").css({
-        "transform": "rotate(" + sizeRotation + "deg) translate3d(0,0,0)",
-        "background-color": color
-    });
-}
-
-const iterateSlices = (sliceSize, pieElement, offset, dataCount, sliceCount, color) => {
-    let sliceID = "s" + dataCount + "-" + sliceCount;
-    let maxSize = 179;
-    if (sliceSize <= maxSize) {
-        addSlice(sliceSize, pieElement, offset, sliceID, color);
-    } else {
-        addSlice(maxSize, pieElement, offset, sliceID, color);
-        iterateSlices(sliceSize - maxSize, pieElement, offset + maxSize, dataCount, sliceCount + 1, color);
-    }
-}
+// const sliceSize = (dataNum, dataTotal) => {
+//     console.log(dataNum, dataTotal);
+//     return (dataNum / dataTotal) * 360;
+// };
+//
+// const addSlice = (sliceSize, pieElement, offset, sliceID, color) => {
+//     $(pieElement).append("<div class='slice " + sliceID + "'><span></span></div>");
+//     // offset = offset - 1;
+//     let sizeRotation = 4.194444444444 * sliceSize;
+//     // $("." + sliceID).css({ "transform": "rotate(0deg) translate3d(0,0,0)" });
+//     $("." + sliceID + " span").css({
+//         "transform": "rotate(0deg) translate3d(0,0,0)",
+//         "background-color": color
+//     });
+// };
+//
+// const iterateSlices = (sliceSize, pieElement, offset, dataCount, sliceCount, color) => {
+//     let sliceID = "s" + dataCount + "-" + sliceCount;
+//     // let maxSize = 179;
+//     // if (sliceSize <= maxSize) {
+//     addSlice(sliceSize, pieElement, offset, sliceID, color);
+//     // } else {
+//     //     addSlice(maxSize, pieElement, offset, sliceID, color);
+//     //     iterateSlices(sliceSize - maxSize, pieElement, offset + maxSize, dataCount, sliceCount + 1, color);
+//     // }
+// };
 
 const createPie = (dataElement, pieElement) => {
     // let dataElement = dataElement;
     // $(dataElement + " span").each(function () {
     //     dataElement.push(Number($(this).html()));
     // });
-    let listTotal = 0;
-    for (let i = 0; i < dataElement.length; i++) {
-        listTotal += dataElement[i];
-    }
+    // let listTotal = 0;
+    // for (let i = 0; i < dataElement.length; i++) {
+    //     listTotal += dataElement[i];
+    // }
 
-    let offset = 0;
-    const color = [
+    // let offset = 0;
+    const colors = [
         "cornflowerblue",
         "olivedrab",
         "orange",
@@ -51,11 +50,74 @@ const createPie = (dataElement, pieElement) => {
         "navy",
         "gray"
     ];
-    for (let i = 0; i < dataElement.length; i++) {
-        const size = sliceSize(dataElement[i], listTotal);
-        iterateSlices(size, pieElement, offset, i, 0, color[i]);
-        // $(dataElement + " li:nth-child(" + (i + 1) + ")").css("border-color", color[i]);
-        offset += size;
-    }
+    draw(dataElement, colors);
+    // for (let i = 0; i < dataElement.length; i++) {
+    //     // const size = sliceSize(dataElement[i], listTotal);
+    //     iterateSlices(dataElement[i][0], pieElement, dataElement[i][1], i, 0, colors[i]);
+    //     // $(dataElement + " li:nth-child(" + (i + 1) + ")").css("border-color", color[i]);
+    //     // offset += size;
+    // }
+};
+
+// createPie([2, 1.5], ".pieID.pie");
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+function degToRad(degrees) {
+    return ((degrees - 90) * Math.PI) / 180;
 }
-createPie([718, 531, 868, 344, 1145], ".pieID.pie");
+
+const draw = (dataElement, colors) => {
+    console.log(dataElement);
+    dataElement = dataElement.sort(sortTableByASC);
+    console.log(dataElement);
+    let radius = 195;
+    let lastDrawEnd = 0;
+
+    for (let i = 0; i < dataElement.length; i++) {
+        const drawStart = convertHoursToFloatNumber(dataElement[i][0]);
+        const drawEnd = convertHoursToFloatNumber(dataElement[i][1]);
+        lastDrawEnd > drawStart ? radius -= 30 : radius = 195;
+        lastDrawEnd = drawEnd;
+
+        ctx.beginPath();
+        ctx.arc(200, 200, radius, degToRad(drawStart), degToRad(drawEnd), false);
+        ctx.fillStyle = colors[i];
+        ctx.lineTo(200, 200);
+        ctx.fill();
+    }
+};
+
+/**
+ *
+ * @param hour
+ * @returns {number}
+ */
+const convertHoursToFloatNumber = (hour) => {
+    const minStart = hour.length - 2;
+    const minEnd = hour.length;
+    const hrStart = hour;
+    const hrEnd = hour.length - 3;
+
+    let min = hour.substring(minStart, minEnd);
+    let hrs = hour.substring(hrStart, hrEnd);
+    if (typeHours === 12 && parseInt(hrs) === 12) hrs = 0;
+    if (typeHours === 12 && parseInt(hrs) > 12) hrs -= 12;
+
+    const ratio = 100 / (100 / 3) * 10;
+    const hrDecimal = Number(hrs + '.' + Math.round(min / 60 * 100));
+    return hrDecimal * ratio;
+};
+
+/**
+ *
+ * @param a
+ * @param b
+ * @returns {number}
+ */
+function sortTableByASC(a, b) {
+    const x = a[0];
+    const y = b[0];
+    return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+}
